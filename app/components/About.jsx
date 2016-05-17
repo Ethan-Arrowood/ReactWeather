604629ef944051dbc8e191db183416c53e99b432
ethan-arrowood <ethan.arrowood@gmail.com>
2016-05-16 20:51:03 -0400

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,10 +3,24 @@ var React = require("react");
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p>This app was build during a UDemy React JS Course. It is a weather app that uses the OpenWeatherMap API.</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - Get React here!
+        </li>
+        <li>
+          Check the <a href="https://www.udemy.com/the-complete-react-web-app-developer-course">course</a> out here!
+        </li>
+        <li>
+          Here is a link to the GitHub repo: <a href="https://github.com/Ethan-Arrowood/ReactWeather">github</a>
+        </li>
+        <li>
+          Open Weather Map: <a href="http://openweathermap.org">click here</a>
+        </li>
+      </ul>
     </div>
-    
+
   )
 };
 module.exports = About;
